e9cd258826960800a68c57218476572c3f4dfda7
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2023-09-06 14:51:15 -0500

    login navbar and first steps on calendar

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,13 +1,35 @@
 "use client";
-import React from "react";
-import Calendar from "@/components/Calendar";
+import React, { useEffect, useState } from "react";
+import Calendar from "@/components/Calendar/Calendar";
 
 function Schedule() {
-  return (
-    <div>
-      <Calendar />
-    </div>
-  );
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const path = window.location.href;
+      console.log("path", path);
+      if (!path.includes("/login")) {
+        validateToken();
+      }
+    }
+  }, []);
+
+  const validateToken = () => {
+    const token = localStorage.getItem("auth");
+    if (token) {
+      setVisible(true);
+    } else {
+      window.location.href = "/login";
+    }
+  };
+
+  if (visible)
+    return (
+      <div>
+        <Calendar />
+      </div>
+    );
 }
 
 export default Schedule;
---
+++ components/Calendar/Calendar.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import esLocale from "@fullcalendar/core/locales/es";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+import "./Calendar.css";
+
+function Calendar() {
+  const createEvent = () => {
+    console.log("Hello");
+  };
+
+  createEvent();
+
+  return (
+    <div className="calendar bg-callejero">
+      <div className="calendar__grid bg-white p-6">
+        <Fullcalendar
+          locale={esLocale}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView={"timeGridWeek"}
+          headerToolbar={{
+            start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+            center: "title",
+            end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+          }}
+          height={"90vh"}
+          // events={["2023-09-04"]}
+          eventColor={"#184135"}
+          events={[
+            { title: "Juego de Practica", date: "2023-09-04T08:00:00" },
+            {
+              title: "Reserva Juan",
+              start: "2023-09-07T12:00:00",
+              end: "2023-09-07T14:00:00",
+            },
+            { title: "Reservado", date: "2023-09-05" },
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
+
+// function renderEventContent(eventInfo: any) {
+//   return (
+//     <>
+//       <b>{eventInfo.timeText}</b>
+//       <i>{eventInfo.event.title}</i>
+//     </>
+//   );
+// }
+
+export default Calendar;
