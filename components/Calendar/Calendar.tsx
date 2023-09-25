403de8de3aff9dd04dff18a0eda3271bec900b26
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2023-09-25 17:59:49 -0500

    adding figma styles and setting up some calendar props

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -18,14 +18,49 @@ function Calendar(data: any) {
           <b>{localStorage.getItem("gamefieldName")}</b>
         </h1> */}
         <Fullcalendar
+          slotDuration="01:00:00"
+          slotLabelFormat={[
+            {
+              hour: "numeric",
+              minute: "2-digit",
+            },
+          ]}
+          slotLabelContent={(slotInfo) => {
+            var hour = slotInfo.date.getHours();
+            var ampm = hour >= 12 ? "PM" : "AM";
+            hour = hour % 12 || 12;
+            return hour + ":00 " + ampm;
+          }}
+          slotLabelClassNames={"slotTimes"}
+          headerToolbar={{
+            start: "prev,next",
+            center: "title",
+            end: "today,dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          titleFormat={{ year: "numeric", month: "long" }}
+          dayHeaderClassNames={"dayHeader"}
+          dayHeaderFormat={{
+            day: "numeric",
+            weekday: "long",
+            omitCommas: true,
+          }}
+          dayHeaderContent={(arg) => {
+            var day = arg.date.getDate(); // Obtener el día del mes
+            var weekday = arg.date.toLocaleString("default", {
+              weekday: "long",
+            });
+            return (
+              <div>
+                <div className="headerDay">{day}</div>
+                <div className="headerWeekday">{weekday}</div>
+              </div>
+            );
+          }}
+          allDayClassNames="clasePrueba"
+          allDaySlot={false}
           locale={esLocale}
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           initialView={"timeGridWeek"}
-          headerToolbar={{
-            start: "today prev,next", // will normally be on the left. if RTL, will be on the right
-            center: "title",
-            end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
-          }}
           height={"calc(100vh - 202px)"}
           eventColor={"#184135"}
           events={bookings.map((booking: any) => ({
