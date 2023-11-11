79c36c9cd3cb070a1f41b2a23b386f130f50971a
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2023-11-11 18:01:49 -0500

    modify map

--- components/ModalEventDetail/index.tsx
+++ components/ModalEventDetail/index.tsx
@@ -30,7 +30,7 @@ const ModalEventDetail: React.FC<{
     dayNumber: string;
     monthName: string;
     description: string;
-    responsables: [];
+    responsables: {};
     totalPrice: number;
     totalPaid: number;
   };
@@ -128,30 +128,31 @@ const ModalEventDetail: React.FC<{
                       Responsables
                     </p>
                     <div className="flex flex-col">
-                      {bookingDetail.responsables.map((responsable: any) => (
-                        <div
-                          key={responsable.id}
-                          className="flex items-center mt-4"
-                        >
+                      {bookingDetail.responsables &&
+                        bookingDetail.responsables.map((responsable: any) => (
                           <div
-                            className={`${
-                              responsable.sex == "m"
-                                ? "bg-blue-300"
-                                : "bg-pink-300"
-                            }  rounded-full flex p-2 mr-2`}
+                            key={responsable.id}
+                            className="flex items-center mt-4"
                           >
-                            <Image
-                              src={responsable.sex == "m" ? male : female}
-                              alt="icon"
-                              height={25}
-                              width={25}
-                              priority={true}
-                              className=""
-                            />
+                            <div
+                              className={`${
+                                responsable.sex == "m"
+                                  ? "bg-blue-300"
+                                  : "bg-pink-300"
+                              }  rounded-full flex p-2 mr-2`}
+                            >
+                              <Image
+                                src={responsable.sex == "m" ? male : female}
+                                alt="icon"
+                                height={25}
+                                width={25}
+                                priority={true}
+                                className=""
+                              />
+                            </div>
+                            <p className="">{responsable.name}</p>
                           </div>
-                          <p className="">{responsable.name}</p>
-                        </div>
-                      ))}
+                        ))}
                     </div>
                   </div>
                   <button
