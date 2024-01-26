3c0ac0718dc8a3549e895021080b5bbc40a45f87
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2024-01-26 18:22:26 -0500

    update env

--- app/globals.tsx
+++ app/globals.tsx
@@ -1,3 +1,4 @@
 export const globals = {
-  apiURL: 'https://callejero.com.co/test/api/v1',
-}
+  // apiURL: 'https://callejero.com.co/test/api/v1',
+  apiURL: "https://callejero.com.co/api/v1",
+};
